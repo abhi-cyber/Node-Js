58e3d6c53f68979e02c4843f467f615059c765bf
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-17 20:18:39 +0530

    Imp & Exp one at a time

---
+++ importExport/index.js
@@ -0,0 +1,6 @@
+// Importing the func. / module
+// const add = require('./oper');
+const name = require('./oper');
+
+// console.log(add(5, 5));
+console.log(name);
---
+++ importExport/oper.js
@@ -0,0 +1,10 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const name = "Abhiraj";
+
+// Exporting the above func/module
+
+// module.exports = add;
+module.exports = name;
