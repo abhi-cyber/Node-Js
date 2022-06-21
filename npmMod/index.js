38537346717706f7967c98a45d57370674e3f7d4
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-21 20:35:07 +0530

    chalk module demo

--- npmMod/index.js
+++ npmMod/index.js
@@ -1,1 +1,6 @@
 // To initalize npm in this folder run the following command in the terminal(in root folder) ---> npm init
+
+import chalk from 'chalk';
+
+console.log(chalk.green.underline.inverse("success"));
+console.log(chalk.red.underline.inverse("fail"));
