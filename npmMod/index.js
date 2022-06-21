afcb2314ccbefa33ba4bf5ec47617a1fdcc4394a
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-22 00:28:59 +0530

    Nodemon installed globaly

--- npmMod/index.js
+++ npmMod/index.js
@@ -11,3 +11,7 @@ import validator from 'validator';
 
 const res = validator.isEmail("abhiraj@chatrath.com");
 console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res));
+
+// ------------------------------------ LIVE VALIDATOR USING NODEMON MODULE ------------------------------
+
+// NODEMON :- It is a global package that automatically restarts the node application when there is change in the code/directory.
