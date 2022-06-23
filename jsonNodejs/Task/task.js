581b8248b014b526cfd422992c18213a91bb884d
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-23 17:26:01 +0530

    Added data to Json File

--- jsonNodejs/Task/task.js
+++ jsonNodejs/Task/task.js
@@ -1,12 +1,19 @@
+const fs = require("fs");
 const bioData = {
-    name : "Abhiraj",
-    age : 19,
-    email: 'abhirajchatrath@gmail.com'
+  name: "Abhiraj",
+  age: 19,
+  email: "abhirajchatrath@gmail.com",
 };
 
 // 1. Convert the obj to JSON. ==> DONE
-// 2. Add it to another file.
+// 2. Add it to another file. ==> Done
 // 3. And at last readFile.
 // 4. Convert back to obj.
 
+// 1
 const jsonData = JSON.stringify(bioData);
+
+// 2
+fs.writeFile("json1.json", jsonData, (err) => {
+  console.log("done");
+});
