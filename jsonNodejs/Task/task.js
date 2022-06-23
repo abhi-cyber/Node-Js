e6c1ace831d453b840d0dfeed7aad3788b9f410b
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-23 17:18:17 +0530

    Task Ques.  (JSON)

---
+++ jsonNodejs/Task/task.js
@@ -0,0 +1,9 @@
+const bioData = {
+    name : "Abhiraj",
+    age : 19,
+    email: 'abhirajchatrath@gmail.com'
+};
+
+// 1. Convert the obj to JSON.
+// 2. Add it to another file.
+// 3. And at last readFile.
