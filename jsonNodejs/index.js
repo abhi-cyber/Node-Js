d3fc0428b9d9903b21335cc666049676723545fb
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-23 17:15:55 +0530

    Convert Obj to JSON

--- jsonNodejs/index.js
+++ jsonNodejs/index.js
@@ -3,3 +3,18 @@
 // for storing and transporting data.
 // 2. JSON is often used when data is sent from a server to a web page.
 
+// ------------------------ CONVERTING OBJECT TO JSON ------------------------------------------------
+
+// Object
+const bioData = {
+    name : "Abhiraj",
+    age : 19,
+    email: 'abhirajchatrath@gmail.com'
+};
+console.log(bioData.name);
+
+// Json format
+const jsonData = JSON.stringify(bioData);
+console.log(jsonData);
+const objData = JSON.parse(jsonData); // will convert the data into obj so that is can be displayed
+console.log(objData.name);
