6b4f96b8c0d93537a616ba5710d70a4c638e211e
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-23 17:15:39 +0530

    JSON in Nodejs

---
+++ jsonNodejs/index.js
@@ -0,0 +1,5 @@
+// Notes : -
+// 1. JSON stands for Javascript Object Notification. JSON is lightweight format 
+// for storing and transporting data.
+// 2. JSON is often used when data is sent from a server to a web page.
+
