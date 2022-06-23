f40c1ade652965012ed4bc245addc8eefba080aa
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-23 17:20:15 +0530

    Convert to JSON

--- jsonNodejs/Task/task.js
+++ jsonNodejs/Task/task.js
@@ -4,7 +4,9 @@ const bioData = {
     email: 'abhirajchatrath@gmail.com'
 };
 
-// 1. Convert the obj to JSON.
+// 1. Convert the obj to JSON. ==> DONE
 // 2. Add it to another file.
 // 3. And at last readFile.
 // 4. Convert back to obj.
+
+const jsonData = JSON.stringify(bioData);
