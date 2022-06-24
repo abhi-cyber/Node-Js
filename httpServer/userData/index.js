fffcdcb5f5924b3098ac532cca84826771d8d88d
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-24 20:40:15 +0530

    Fix for multiple page reloading

--- httpServer/userData/index.js
+++ httpServer/userData/index.js
@@ -2,7 +2,9 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // console.log(req.url)
+  const data = fs.readFileSync("../UserApi/userapi.json", "utf-8");
+  const objData = JSON.parse(data);
+
   if (req.url == "/") {
     res.end("This is the Home Page");
   } else if (req.url == "/about") {
@@ -10,12 +12,14 @@ const server = http.createServer((req, res) => {
   } else if (req.url == "/contact") {
     res.end("This is the ContactUs Page.");
   } else if (req.url == "/userapi") {
-    fs.readFile("../UserApi/userapi.json", "utf-8", (err, data) => {
-      console.log(data);
-      // Code to call a specific field from the data first convert it into an object.
-      const objData = JSON.parse(data);
-      res.end(objData[0].name);
-    });
+    // fs.readFile("../UserApi/userapi.json", "utf-8", (err, data) => {
+    //   console.log(data);
+    //   // Code to call a specific field from the data first convert it into an object.
+    //   const objData = JSON.parse(data);
+    //   res.end(objData[0].name);
+    // });
+    res.writeHead(200, {"content-type" : "application/json"})
+    res.end(objData[0].name);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<h1>404 ERROR. Page does not exist.</h1>");
