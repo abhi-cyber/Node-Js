d662e6f9e8a81d6bc072a6f4b7ce25d691227d3e
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-25 13:46:27 +0530

    Using Stream

--- StreamModule/index.js
+++ StreamModule/index.js
@@ -9,10 +9,26 @@ const server = http.createServer();
 server.on('request', (req, res) => {
     // Using file module --> 
 
-    fs.readFile('input.txt', function (err, data) {
-      if (err) return console.error(err);
-      res.end(data.toString());
+    // fs.readFile('input.txt', function (err, data) {
+    //   if (err) return console.error(err);
+    //   res.end(data.toString());
+    // });
+
+    // Using Stream module
+
+    const rstream = fs.createReadStream('input.txt');
+    
+    rstream.on('data', (chunkdata) => {
+        res.write(chunkdata)
     });
+    rstream.on('end', () => {
+        res.end();
+    });
+    rstream.on('error', (err) => {
+        console.log(err);
+        res.end("File not found.");
+    });
+
 });
 
 server.listen(8000, "127.0.0.1");
