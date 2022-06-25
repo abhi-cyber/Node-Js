bbc2aa58c6d344ac24aab8b6bab237a8017c8f66
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-25 13:23:37 +0530

    Example 3

--- EventMod/index.js
+++ EventMod/index.js
@@ -2,8 +2,8 @@
 // Node.js has a built-in module, called "Event".
 // where you can create-, fire-, and listen for- your own events.
 
-const EventEmitter = require('events');
-const http = require('http');
+const EventEmitter = require("events");
+const http = require("http");
 const event = new EventEmitter(); // Obj. created
 
 // Example 1:- Registering for the event to be fired only one time using once.
@@ -18,16 +18,23 @@ const event = new EventEmitter(); // Obj. created
 
 // Example 2:- Create an event emitter instance and register a couple of callbacks.
 
-event.on("whatsMyName", () => { // ".on" is a listner
-    console.log("Your name is Abhiraj.");
-});
+// event.on("whatsMyName", () => { // ".on" is a listner
+//     console.log("Your name is Abhiraj.");
+// });
 
-event.on("whatsMyName", () => { // ".on" is a listner
-    console.log("Your name is Singh.");
-});
+// event.on("whatsMyName", () => { // ".on" is a listner
+//     console.log("Your name is Singh.");
+// });
 
-event.on("whatsMyName", () => { // ".on" is a listner
-    console.log("Your name is Chatrath.");
-});
+// event.on("whatsMyName", () => { // ".on" is a listner
+//     console.log("Your name is Chatrath.");
+// });
 
-event.emit("whatsMyName");
+// event.emit("whatsMyName");
+
+// Example 3:- Registering for the event with callback parameters
+
+event.on("checkPage", (sc, msg) => {
+    console.log(`status code is ${sc} and the page is ${msg}.`);
+})
+event.emit("checkPage", 200, 'ok');
