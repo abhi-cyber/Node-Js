d21491e53155e508e57411e6ded4f0ab460d67a9
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-25 13:16:09 +0530

    Example 1

--- EventMod/index.js
+++ EventMod/index.js
@@ -8,3 +8,10 @@ const event = new EventEmitter(); // Obj. created
 
 // Example 1:- Registering for the event to be fired only one time using once.
 
+event.on("whatsMyName", () => { // ".on" is a listner
+    console.log("Your name is Abhiraj.");
+});
+
+event.emit("whatsMyName");
+
+// Note:- First you need to set the listner and then emit it or it will not work.
