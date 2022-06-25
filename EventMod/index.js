91ffa95c5443dbd7489cde6f808f529f9427d4e9
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-25 13:10:09 +0530

    Mod. required

--- EventMod/index.js
+++ EventMod/index.js
@@ -2,3 +2,9 @@
 // Node.js has a built-in module, called "Event".
 // where you can create-, fire-, and listen for- your own events.
 
+const EventEmitter = require('events');
+const http = require('http');
+const event = new EventEmitter(); // Obj. created
+
+// Example 1:- Registering for the event to be fired only one time using once.
+
